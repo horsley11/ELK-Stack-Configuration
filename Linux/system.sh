5d40446becb6bb967bfc8f7979b5939d1715ac79
Benjamin Horsley <77799265+horsley11@users.noreply.github.com>
2021-06-06 02:24:42 -0400

    Add files via upload

---
+++ Linux/system.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+free men -h > ~/backups/freeman/free_men.txt
+du -h > ~/backups/diskuse/disk_usage.txt
+lsof /dev/null > ~/backups/openlist/open_list.txt
+df -h > ~/backups/freedisk/free_disk.txt
